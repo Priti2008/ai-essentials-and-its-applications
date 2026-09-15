e9617654458a95b8df9f7fe0d99b2488e450a856
Priti2008 <p6102267@gmail.com>
2026-09-15 10:44:58 +0530

    basic agent

---
+++ basic_agent.py
@@ -0,0 +1,35 @@
+def model_based_vacuum(location, status, memory):
+
+
+    if status == "Dirty":
+        memory[location] = "Clean"
+        return "Clean"
+
+
+    if status == "Clean":
+        memory[location] = "Clean"
+
+
+    if location == "A":
+        if memory["B"] == "Dirty":
+            return "Move to B"
+        else:
+            return "Do nothing"
+
+    else:
+        if memory["A"] == "Dirty":
+            return "Move to A"
+        else:
+            return "Do nothing"
+
+
+memory = {
+    "A": "Unknown",
+    "B": "Unknown"
+}
+
+print(model_based_vacuum("A", "Dirty", memory))
+print(memory)
+
+print(model_based_vacuum("B", "Clean", memory))
+print(memory)
