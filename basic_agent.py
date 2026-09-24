9f7b8c0fe3fa89b25812ad0267185261f6d8bf21
Priti2008 <p6102267@gmail.com>
2026-09-24 14:38:14 +0530

    basicagect

--- basic_agent.py
+++ basic_agent.py
@@ -1,4 +1,73 @@
+# # def model_based_vacuum(location, status, memory):
+
+
+# #     if status == "Dirty":
+# #         memory[location] = "Clean"
+# #         return "Clean"
+
+
+# #     if status == "Clean":
+# #         memory[location] = "Clean"
+
+
+# #     if location == "A":
+# #         if memory["B"] == "Dirty":
+# #             return "Move to B"
+# #         else:
+# #             return "Do nothing"
+
+# #     else:
+# #         if memory["A"] == "Dirty":
+# #             return "Move to A"
+# #         else:
+# #             return "Do nothing"
+
+
+# # memory = {
+# #     "A": "Unknown",
+# #     "B": "Unknown"
+# # }
+
+# # print(model_based_vacuum("A", "Dirty", memory))
+# # print(memory)
+
+# # print(model_based_vacuum("B", "Clean", memory))
+# # print(memory)
 # def model_based_vacuum(location, status, memory):
+
+
+#     if status == "Dirty":
+#         memory[location] = "Clean"
+#         return "Clean"
+
+
+#     if status == "Clean":
+#         memory[location] = "Clean"
+
+
+#     if location == "A":
+#         if memory["B"] == "Dirty":
+#             return "Move to B"
+#         else:
+#             return "Do nothing"
+
+#     else:
+#         if memory["A"] == "Dirty":
+#             return "Move to A"
+#         else:
+#             return "Do nothing"
+
+
+# memory = {
+#     "A": "Unknown",
+#     "B": "Unknown"
+# }
+
+# print(model_based_vacuum("A", "Dirty", memory))
+# print(memory)
+
+# print(model_ba
+# # def model_based_vacuum(location, status, memory):
 
 
 #     if status == "Dirty":
@@ -67,4 +136,5 @@ print(model_based_vacuum("A", "Dirty", memory))
 print(memory)
 
 print(model_based_vacuum("B", "Clean", memory))
-print(memory)
+print(memory)sed_vacuum("B", "Clean", memory))
+# print(memory)
